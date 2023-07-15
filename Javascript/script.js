3bfe034988ec369d0a7c68264847c801c5059fba
33aodan <dan.wnrs@gmail.com>
2023-07-15 11:18:33 -0500

    paap

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,7 +8,41 @@
 // 'python'
 
 // variables can change value
-let ageA = 15;
-console.log(ageA);
+// let ageA = 15;
+// console.log(ageA);
 
 // naming conventions to name a variable:
+
+// var age =15;
+// age =15;
+// console.log(age);
+
+// we dont use data types to declare a variable in javascript as like c++
+
+// type of is a keyword in javascript which is used to know the datatype of a variable
+
+// var age =15;
+// age =15;
+// console.log(typeof age);
+
+// number : decimals and integers
+// undefined : if im not putting any Value
+// null : empty value
+// bigint: too big and does not get adjusted in number data type
+// string: letters and words in qoutes
+// boolean: true/false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
