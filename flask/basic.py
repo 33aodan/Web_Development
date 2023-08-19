f35e3633e543c35e74aa383aa8747063c567a69d
33aodan <dan.wnrs@gmail.com>
2023-08-19 12:55:19 -0500

    sad

---
+++ flask/basic.py
@@ -0,0 +1,9 @@
+isuppercase = bool(input("do you want uppercase letters?"))
+isnumbers = bool(input("do you want numbers?"))
+issymbols = bool(input("do you want symbols?"))
+
+print(isuppercase)
+print(isnumbers)
+print(issymbols)
+
+choices = list("abcdefghijkmlopqrstuvxyz")
