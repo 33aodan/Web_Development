cd705f31fd1102f67feb70cf1391b3369197cab6
33aodan <dan.wnrs@gmail.com>
2023-08-27 14:21:15 -0500

    sad

--- flask/basic.py
+++ flask/basic.py
@@ -1,9 +1,37 @@
-isuppercase = bool(input("do you want uppercase letters?"))
-isnumbers = bool(input("do you want numbers?"))
-issymbols = bool(input("do you want symbols?"))
+# isuppercase = bool(input("do you want uppercase letters?"))
+# isnumbers = bool(input("do you want numbers?"))
+# issymbols = bool(input("do you want symbols?"))
+# length = int(input("How many characters do you want?"))
 
-print(isuppercase)
-print(isnumbers)
-print(issymbols)
+# print(isuppercase)
+# print(isnumbers)
+# print(issymbols)
+# import random
+# def generate_password():
+#     choices = list("abcdefghijkmlopqrstuvxyz")
 
-choices = list("abcdefghijkmlopqrstuvxyz")
+#     if isuppercase == True:
+#         choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
+#     if isnumbers == True:
+#         choices.extend("0123456789")
+#     if issymbols == True:
+#         choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
+
+#     mypassword = ""
+#     for i in range(length):
+#         mypassword += random.choice(choices)
+
+#     print(mypassword)
+# generate_password()
+
+import random
+from flask import render_template,Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
