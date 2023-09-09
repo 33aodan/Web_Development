2f17d3a8dd7ba1a141df567ed68e21570ef597d1
33aodan <dan.wnrs@gmail.com>
2023-09-09 12:01:10 -0500

    asd

--- flask/basic.py
+++ flask/basic.py
@@ -1,31 +1,5 @@
-# isuppercase = bool(input("do you want uppercase letters?"))
-# isnumbers = bool(input("do you want numbers?"))
-# issymbols = bool(input("do you want symbols?"))
-# length = int(input("How many characters do you want?"))
-
-# print(isuppercase)
-# print(isnumbers)
-# print(issymbols)
-# import random
-# def generate_password():
-#     choices = list("abcdefghijkmlopqrstuvxyz")
-
-#     if isuppercase == True:
-#         choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
-#     if isnumbers == True:
-#         choices.extend("0123456789")
-#     if issymbols == True:
-#         choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
-
-#     mypassword = ""
-#     for i in range(length):
-#         mypassword += random.choice(choices)
-
-#     print(mypassword)
-# generate_password()
-
-import random
-from flask import render_template,Flask
+import random 
+from flask import render_template, Flask, request  
 
 app = Flask(__name__)
 
@@ -33,5 +7,35 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/fill_form')
+def fill_form():
+    return render_template('fill_form.html')
+
+@app.route('/password')
+def password():
+    choices = list("abcdefghijklmnopqrstuvwxyz")
+
+    isUppercase = request.args.get("uppercase")
+    isNumbers = request.args.get("numbers")
+    isSymbols = request.args.get("symbols")
+    length = request.args.get("length")
+
+    if int(length) == None: 
+        length = 8 
+    if isUppercase == '1': 
+        choices.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    if isNumbers == '1':
+        choices.extend("0123456789")
+    if isSymbols == '1':
+        choices.extend('''~!@#$%^&*()_+-=){}[]\|:;"',.<>?/]''')
+
+    mypassword = ""
+    for i in range(int(length)):
+        mypassword += random.choice(choices)
+
+    return render_template('password.html', mypassword=mypassword)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
